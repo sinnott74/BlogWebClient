f46576aec261cc228ff41c86ad7aebae759bf6f7
Daniel Sinnott <sinnott74@hotmail.com>
2018-10-24 18:19:12 +0100

    Fix globbing pattern

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,15 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy("/api/todos", { target: "http://localhost:8000", secure: false })
+    proxy("/api/todos/**", {
+      target: "http://localhost:8000",
+      secure: false
+    })
   );
-  app.use(proxy("/api/*", { target: "http://localhost:8080", secure: false }));
+  app.use(
+    proxy("/api/**", {
+      target: "http://localhost:8080",
+      secure: false
+    })
+  );
 };
