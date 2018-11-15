e5b9857e48f33dd98b49ba0197e542a4a48eaf98
Daniel Sinnott <sinnott74@hotmail.com>
2018-11-15 15:55:14 +0000

    Add canonical link header

--- src/blog/pages/ViewBlogPostPage.jsx
+++ src/blog/pages/ViewBlogPostPage.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ViewBlogPost from "blog/containers/ViewBlogPost";
+import Helmet from "react-helmet";
 
 const ViewBlogPostPage = props => {
-  return <ViewBlogPost id={props.match.params.id} showActions />;
+  console.log();
+  return (
+    <React.Fragment>
+      <Helmet>
+        <link
+          rel="canonical"
+          href={`https://${window.location.hostname}/blog/${
+            props.match.params.id
+          }`}
+        />
+      </Helmet>
+      <ViewBlogPost id={props.match.params.id} showActions />
+    </React.Fragment>
+  );
 };
 
 ViewBlogPostPage.propTypes = {
